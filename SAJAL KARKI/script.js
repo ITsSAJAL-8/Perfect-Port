4b7c7c9154043d7210f1c96c3577060902b39a80
ITsSAJAL-8 <sajal.karki05@gmail.com>
2026-05-23 15:40:38 +0545

    oky

--- SAJAL KARKI/script.js
+++ SAJAL KARKI/script.js
@@ -7,7 +7,7 @@ menuIcon.onclick = () => {
     navbar.classList.toggle('active');
 }
 
-// scroll sections
+
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
 
@@ -19,31 +19,29 @@ window.onscroll = () => {
         let id = sec.getAttribute('id');
 
         if(top >= offset && top < offset + height) {
-            // active navbar links
+            
             navLinks.forEach(links => {
                 links.classList.remove('active');
                 document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
             });
-            // active sections for animation on scroll
+            
             sec.classList.add('show-animate');
         }
-        // if want to animation that repeats on scroll use this
+      
         else {
             sec.classList.remove('show-animate');
         }
     });
 
-    // sticky navbar
+    
     let header = document.querySelector('header');
 
     header.classList.toggle('sticky', window.scrollY > 100);
 
-    // remove toggle icon and navbar when click navbar links (scroll)
+   
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
-    // animation footer on scroll
-    let footer = document.querySelector('footer');
-
-    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
+ 
+    
 }
